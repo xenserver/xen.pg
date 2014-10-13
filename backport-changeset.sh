bc5c160ed74c524b17074cfc63ab874707ddebf5
Andrew Cooper <andrew.cooper3@citrix.com>
2014-10-13 16:51:03 +0000

    Script to help generate backports

---
+++ backport-changeset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+#
+# Helper script to backport changesets
+#
+# Patch name: backport-<12 digit SHA>.patch
+# Series entry: <patchname> #   <UTC date> - <commit subject>
+#
+
+set -o pipefail
+
+TOP=`git rev-parse --show-toplevel`
+cd $TOP || { echo >&2 "Failed to chdir to $TOP"; exit 1; }
+
+SYMREF=`git symbolic-ref HEAD`
+BRANCH=${SYMREF##*/}
+
+while [[ $# -gt 0 ]]; do
+    echo "Considering $1"
+
+    SHORTSHA=`git log -n1 $1 --abbrev=12 --pretty=format:"%h"`
+    SUBJECT=`git log -n1 $1 --pretty=format:"%s"`
+
+    # Obtain date in absolute UTC without a Timezone shift
+    DATE=`date -d @$(TZ=UTC git log -n1 --format="%ct" $1) "+%Y-%m-%d %T"`
+
+    git format-patch -N1 --stdout > .git/patches/$BRANCH/backport-$SHORTSHA.patch
+    echo "backport-$SHORTSHA.patch #   $DATE - $SUBJECT" >> .git/patches/$BRANCH/series.snippet
+
+    shift
+done
