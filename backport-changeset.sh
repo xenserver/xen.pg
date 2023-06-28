6f65ab24168a886766ff792defac5ddc21d0bf32
Andrew Cooper <andrew.cooper3@citrix.com>
2023-06-28 20:22:54 +0100

    Improvements to backport-changeset.sh
     * Pass --no-base to git format-patch to suppress base-commit insertions
     * Support ranges using git rev-list
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- backport-changeset.sh
+++ backport-changeset.sh
@@ -15,17 +15,29 @@ cd $TOP || { echo >&2 "Failed to chdir to $TOP"; exit 1; }
 SYMREF=`git symbolic-ref HEAD`
 BRANCH=${SYMREF##*/}
 
-while [[ $# -gt 0 ]]; do
-    echo "Considering $1"
-
+emit () {
     SHORTSHA=`git log -n1 $1 --abbrev=12 --pretty=format:"%h"`
     SUBJECT=`git log -n1 $1 --pretty=format:"%s"`
 
     # Obtain date in absolute UTC without a Timezone shift
     DATE=`date -d @$(TZ=UTC git log -n1 --format="%ct" $1) "+%Y-%m-%d %T"`
 
-    git format-patch -kp --no-signature $1 -N1 --stdout > .git/patches/$BRANCH/backport-$SHORTSHA.patch
+    git format-patch -kp --no-signature --no-base $1 -N1 --stdout > .git/patches/$BRANCH/backport-$SHORTSHA.patch
     echo "backport-$SHORTSHA.patch #   $DATE - $SUBJECT" >> .git/patches/$BRANCH/snippet-for-series
+}
+
+while [[ $# -gt 0 ]]; do
+    echo "Considering $1"
+
+    if [[ $1 =~ ^[0-9a-f]{4,40}$ ]]; then
+        # Single revision
+        emit $1
+    else
+        # Something more complex.  Ask `git rev-list`
+        for R in `git rev-list $1`; do
+            emit $R
+        done
+    fi
 
     shift
 done
